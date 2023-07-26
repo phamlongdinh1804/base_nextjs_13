17aa9ce0856008121a48f9fa6df5bc1341a4cba1
Đĩnh Phạm Long <phamlongdinh1804@gmail.com>
2023-07-26 23:43:59 +0700

    Change HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import Layout from '@/layouts/main'
-import { store, RootState, AppDispatch } from '@/store'
-
-import { updateUsername } from '@/store/userSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+import Image from 'next/image'
+import image_1 from '@assets/images/image_1.png'
 
 export default function HomePage({ res }: any) {
   return (
     <>
       <div className="flex justify-center items-center h-full">
-        <div className="font-medium text-[3rem]">Next.js 13 BASE: Pages Router With Tailwindcss, Antd, Redux</div>
+        <div>
+          <Image src={image_1} width={500} height={500} alt="Picture of the author" />
+        </div>
       </div>
     </>
   )
